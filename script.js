12cec9735b740bef9f1d10bf7cd6846d935ffc1d
Prithvi Raj Singh <prithviraj.singh155171@gmail.com>
2026-08-20 12:46:38 +0530

    DOM BULB Project2

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 let bulb = document.querySelector('.bulb');
-let onbutton = document.querySelector('#on');
-let offbutton = document.querySelector('#off');
+let button = document.querySelector('button');
 
+let flag = 0;
 
-onbutton.addEventListener('click',function(){
-  bulb.style.backgroundColor = 'Gold';
+button.addEventListener('click',function(){
+  if(flag === 0){
+    bulb.style.backgroundColor = 'Gold';
+    flag = 1;
+  }else{
+    bulb.style.backgroundColor = 'white';
+    flag = 0;
+  }
+  
 });
 
-offbutton.addEventListener('click',function(){
-  bulb.style.backgroundColor = 'white';
-});
