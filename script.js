7ecfeab4396d71784a12533f28f97484bb9fa827
Prithvi Raj Singh <prithviraj.singh155171@gmail.com>
2026-08-20 11:22:18 +0530

    DOM

--- script.js
+++ script.js
@@ -3,13 +3,14 @@ let increase = document.querySelector('#Increase');
 let decrease = document.querySelector('#Decrease');
 
 let count = 0;
+
 increase.addEventListener('click',function(){
   count++;
   counter.innerHTML = count;
-})
-
+});
 
 decrease.addEventListener('click',function(){
   count--;
   counter.innerHTML = count;
-})
+});
+
