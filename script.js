8817ca44931e361ba2f76f697e476de1a6c76fee
Prithvi Raj Singh <prithviraj.singh155171@gmail.com>
2026-08-21 14:04:29 +0530

    Team Predictor Project DOM

--- script.js
+++ script.js
@@ -1,12 +1,66 @@
-let box = document.querySelector('#box');
+let box = document.querySelector('main');
+let boxText = document.querySelector('main h1');
 let button = document.querySelector('button');
 
-
+let arr = [
+  {
+    'team' : 'CSK',
+    'Primary' : 'Yellow',
+    'Secondary' : 'Blue'
+  },
+  {
+    'team' : 'MI',
+    'Primary' : 'Blue',
+    'Secondary' : 'Gold'
+  },
+  {
+    'team' : 'KKR',
+    'Primary' : 'Purple',
+    'Secondary' : 'Gold'
+  }
+  ,{
+    'team' : 'RCB',
+    'Primary' : 'Red',
+    'Secondary' : 'black'
+  },
+  {
+    'team' : 'SRH',
+    'Primary' : 'Orange',
+    'Secondary' : 'black'
+  },
+  {
+    'team' : 'PBSK',
+    'Primary' : 'Red',
+    'Secondary' : 'Blue'
+  },
+  {
+    'team' : 'RR',
+    'Primary' : 'Pink',
+    'Secondary': 'Blue'
+  },
+  {
+    'team' : 'DD',
+    'Primary': 'Blue',
+    'Secondary': 'Red'
+  },
+  {
+    'team' : 'LSG',
+    'Primary':'Aqua',
+    'Secondary':'orange'
+  },
+  {
+    'team' : 'GT',
+    'Primary':'Black',
+    'Secondary':'Gold'
+  }
+];
 
 button.addEventListener('click',function(){
-  let red = Math.floor(Math.random()*100);
-  let green = Math.floor(Math.random()*100);
-  let blue = Math.floor(Math.random()*100);
+  let guess = arr[Math.floor(Math.random()*10)];
 
-  box.style.backgroundColor = `rgb(${red},${green},${blue})`;
+  boxText.innerHTML = `${guess.team}`;
+  boxText.style.color = `${guess.Secondary}`;
+  box.style.backgroundColor = `${guess.Primary}`;
+  console.log(guess);
+
 })
